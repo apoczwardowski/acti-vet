d87d68317e2131ff6e56909cba8e130f8eedb1de
apoczwardowski <artur.poczwardowski01@gmail.com>
2022-08-23 08:11:10 +0200

    Edited some styling and prepared Heroimage component

---
+++ src/Components/Hero.js
@@ -0,0 +1,11 @@
+import HeroImage from 'src/Images/small-dog-vet.jpg'
+
+const Hero = () => {
+    return (
+        <div className="hero-image">
+            <img src={HeroImage} />
+        </div>
+    );
+}
+ 
+export default Hero;
